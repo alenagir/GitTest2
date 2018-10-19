de3626650e994f4fdab53f3fe200c96c7ceadc5a
Artur Chesnokov <artur.chesnokov@gmail.com>
2018-10-19 11:49:25 +0300

    Input/Output

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -46,6 +49,38 @@ public class GitTest {
         Arrays.sort(arr, Collections.reverseOrder());
         int max = arr[0];
         return max;
+    }
+
+     /**
+     * Method #1.
+     * Returns array of Integers after manual input from console
+     * Manual input can be stopped by entering non numeric value
+     * @author Chesnokov A.
+     */
+    public Integer[] input(int size) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Integer[] array = new Integer[size];
+        int temp;
+        for (int i = 0; i < size; i++) {
+            try {
+                temp = Integer.parseInt(reader.readLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Non numeric value, end of manual initialization");
+                break;
+            }
+            array[i] = temp;
+        }
+        return array;
+    }
+
+    /**
+     * Method #2.
+     * Returns array as a string.
+     *
+     * @author Chesnokov A.
+     */
+    public String print() {
+        return Arrays.toString(this.arr);
     }
 
 
