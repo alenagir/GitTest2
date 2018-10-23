36c1621d785f0c15247ef2ac2757c7acb9783d29
Alona <krupnova.a.i@gmail.com>
2018-10-23 17:24:05 +0300

    +init

--- src/GitTest.java
+++ src/GitTest.java
@@ -88,7 +88,7 @@ public class GitTest {
         }
     }
 
-}
+
 
      /**
      * Method #14.
@@ -137,7 +137,7 @@ public class GitTest {
     public String arrayFieldAsString() {
         return Arrays.toString(this.arr);
     }
-}
+
 
     /**
      * Method #4.
@@ -153,5 +153,25 @@ public class GitTest {
         }
         return result;
     }
+
+
+
+    /**
+     * Method #14.
+     * Initializes n elements of the array.
+     *
+     * @author Hrytsiuk A.
+     */
+
+    public int[] init(int n){
+
+        for (int i = 0; i < n; i++){
+            arr[i] =(int)(Math.random()*10);
+        }
+        return arr;
+
+    }
+
+
 
 }
